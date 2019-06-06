c3e2b6fe8076c47240cd70e97533f6a65eafb44a
Nofar Peled <Nofarpeled4@gmail.com>
2019-06-06 18:13:28 +0300

    changed text function

--- js/meme-editor-service.js
+++ js/meme-editor-service.js
@@ -1,7 +1,7 @@
 'use strict'
 
-let gCurrMeme = {
-    image: '01',
+let gMeme = {
+    image: 'imgs/004.jpg',
     text: 'text',
     fontSize: '16',
     fontColor: 'rgb(0, 0, 0)',
@@ -11,42 +11,61 @@ let gCurrMeme = {
     textBorder: 'none',
 }
 
+function createMeme(){
+    var meme = {
+        image: 'imgs/004.jpg',
+        text: 'text',
+        fontSize: '16',
+        fontColor: 'rgb(0, 0, 0)',
+        fontFamily: 'eurofurence',
+        fontStyle: 'none',
+        textLocation: {x:'0', y:'0'},
+        textBorder: 'none',
+    }
+    return meme;
+}
 //-----------------------------------------------
 
 //FUNCTION - USER EDIT MEME 
 
 function changeImage(img) {
-    gCurrMeme.image = img;
+    gMeme.image = img;
+
 }
 
 function changeText(str) {
-    gCurrMeme.text = str;
+    gMeme.text = str;
 }
 
 function changeFontSize(size){
-    gCurrMeme.fontSize = size;
+    gMeme.fontSize = size;
+
 }
 
 function changeColor(color){ 
-    gCurrMeme.fontColor = color;
+    gMeme.fontColor = color;
 }
 
 function changeFont(font) {
-    gCurrMeme.fontFamily = font;
+    gMeme.fontFamily = font;
 }
 
 function changeStyle(style) {
-    gCurrMeme.fontStyle = style;
+    gMeme.fontStyle = style;
 }
 
 function changeBorder(border) {
-    gCurrMeme.textBorder = border;
+    gMeme.textBorder = border;
+    console.log(gMeme)
 }
 
 function getMemeProp(){
-    return gCurrMeme;
+    return gMeme;
 }
 
+function changeImage(imgPath){
+    gMeme.image = imgPath;
+}
 
 //------------------------------------------------
 
